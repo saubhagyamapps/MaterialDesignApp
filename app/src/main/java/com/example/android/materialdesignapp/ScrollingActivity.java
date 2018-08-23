fb42f869d2ded20f4f96d64fa79184fbabc6ad67
keshu <keshuvodedara@gmail.com>
2018-08-23 11:08:40 +0530

    a

--- app/src/main/java/com/example/android/materialdesignapp/ScrollingActivity.java
+++ app/src/main/java/com/example/android/materialdesignapp/ScrollingActivity.java
@@ -1,34 +1,23 @@
 package com.example.android.materialdesignapp;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
-import android.support.design.widget.FloatingActionButton;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
-import android.widget.HorizontalScrollView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-
-import de.hdodenhof.circleimageview.CircleImageView;
+import android.widget.RelativeLayout;
 
 public class ScrollingActivity extends AppCompatActivity {
     private static final String TAG = "ScrollingActivity";
     CollapsingToolbarLayout collapsingToolbar;
-    TextView txtName;
-    CardView card_view;
-    View view;
-    FloatingActionButton fab;
-    AppBarLayout linearLayout;
-    CircleImageView profile_image;
-    HorizontalScrollView scrollView;
-    LinearLayout layout;
-    Toolbar toolbar3;
+    DrawerLayout drawerLayout;
+    RecyclerView pastorder_recycler;
+    RelativeLayout relativeLayout;
 
 
     @Override
@@ -36,16 +25,14 @@ public class ScrollingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrolling);
         collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
-        fab = (FloatingActionButton) findViewById(R.id.fab);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-
-        card_view = (CardView) findViewById(R.id.toolbar3);
-        layout = (LinearLayout) findViewById(R.id.linearLayout);
         setSupportActionBar(toolbar);
         AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.app_bar);
-        scrollView = (HorizontalScrollView) findViewById(R.id.horizontalScroll1);
 
-        RecyclerView pastorder_recycler = findViewById(R.id.rd);
+
+        pastorder_recycler = findViewById(R.id.rd);
         final LinearLayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
         layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         pastorder_recycler.setLayoutManager(layoutManager);
@@ -53,6 +40,7 @@ public class ScrollingActivity extends AppCompatActivity {
         pastorder_recycler.setAdapter(pastOrdersAdapter);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
 
         appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
             @Override
@@ -62,25 +50,27 @@ public class ScrollingActivity extends AppCompatActivity {
                 Log.e(TAG, "onOffsetChanged: ");
                 if (Math.abs(verticalOffset) - appBarLayout.getTotalScrollRange() == 0) {
                     Log.e(TAG, "onOffsetChanged:2");
-
-
-                    card_view.post(new Runnable() {
+                    relativeLayout.post(new Runnable() {
                         @Override
                         public void run() {
+                            final DrawerLayout.LayoutParams layoutparams = (DrawerLayout.LayoutParams) relativeLayout.getLayoutParams();
+                            layoutparams.setMargins(0, 0, 0, 0);
+                            relativeLayout.setLayoutParams(layoutparams);
                             collapsingToolbar.setTitle("Mcdonalds");
-                            card_view.setVisibility(View.GONE);
                         }
                     });
-                } else {
 
-                    card_view.post(new Runnable() {
+                } else {
+                    relativeLayout.post(new Runnable() {
                         @Override
                         public void run() {
-                            card_view.setVisibility(View.VISIBLE);
+                            final DrawerLayout.LayoutParams layoutparams = (DrawerLayout.LayoutParams) relativeLayout.getLayoutParams();
+                            layoutparams.setMargins(0, 210, 0, 0);
+                            relativeLayout.setLayoutParams(layoutparams);
                             collapsingToolbar.setTitle(" ");
-
                         }
                     });
+
 
                     Log.e(TAG, "onOffsetChanged:3");
                 }
